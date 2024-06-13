f505747f6edb2036550f6cb87507e836cf850c8c
kleiry20 <74005559+kleiry20@users.noreply.github.com>
2024-06-13 16:47:28 +0530

    working on js

--- 06 - Ajax Type Ahead/index.js
+++ 06 - Ajax Type Ahead/index.js
@@ -1,1 +1,36 @@
-const a = document.getElementById();
+const endpoint =
+  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+
+const cities = [];
+fetchData();
+// fetch(endpoint)
+//   .then((res) => res.json())
+//   .then((data) => {
+//     cities.push(...data);
+//   });
+
+async function fetchData() {
+  const res = await fetch(endpoint);
+  const result = await res.json();
+  cities.push(...result);
+  //   console.log('resss', cities)
+}
+
+if (cities != undefined) {
+  const searchParam = document.getElementById("searchInput");
+  searchParam.addEventListener("change", findMatches(cities));
+}
+
+function findMatches(cities) {
+  console.log("im called");
+  const searchVal = document.getElementById("searchInput").value;
+  console.log("searchVal", searchVal);
+
+  console.log("resss", cities);
+  const filteredCities = cities.filter((cities) =>
+    console.log("cities", cities)
+  );
+  console.log("filteredcities", filteredCities);
+}
+
+// findMatches();
