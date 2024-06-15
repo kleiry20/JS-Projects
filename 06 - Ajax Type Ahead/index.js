55255b6b30734cd49111cbf1478747d26c47291c
kleiry20 <74005559+kleiry20@users.noreply.github.com>
2024-06-15 23:17:30 +0530

    search filter

--- 06 - Ajax Type Ahead/index.js
+++ 06 - Ajax Type Ahead/index.js
@@ -2,6 +2,7 @@ const endpoint =
   "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
 
 const cities = [];
+// const filteredCities = [];
 fetchData();
 // fetch(endpoint)
 //   .then((res) => res.json())
@@ -13,24 +14,41 @@ async function fetchData() {
   const res = await fetch(endpoint);
   const result = await res.json();
   cities.push(...result);
-  //   console.log('resss', cities)
 }
 
-if (cities != undefined) {
-  const searchParam = document.getElementById("searchInput");
-  searchParam.addEventListener("change", findMatches(cities));
+// function handleChange() {
+//   if (cities != undefined) {
+//     const searchParam = document.getElementById("searchInput");
+//     searchParam.addEventListener("change", findMatches(cities));
+//   }
+// }
+
+function findMatches(searchVal, cities) {
+  const filterRes = cities.filter((city) => {
+    if (city.city.toLowerCase().includes(searchVal)) {
+      return city;
+    }
+  });
+
+  return filterRes;
 }
 
-function findMatches(cities) {
-  console.log("im called");
-  const searchVal = document.getElementById("searchInput").value;
-  console.log("searchVal", searchVal);
+// display function
+const searchInput = document.querySelector(".search");
+const suggestions = document.querySelector(".suggestions");
 
-  console.log("resss", cities);
-  const filteredCities = cities.filter((cities) =>
-    console.log("cities", cities)
-  );
-  console.log("filteredcities", filteredCities);
+searchInput.addEventListener("change", displayMatches);
+searchInput.addEventListener("keyup", displayMatches);
+
+function displayMatches() {
+  const filteredCities = findMatches(searchInput.value, cities);
+  console.log("res", filteredCities);
+//   const html = filteredCities.map((place) => {
+//     return `
+//     <li>
+//         <span class='name'>${place.city}, ${place.state}</span>
+//         <span class='population'>${place.city}, ${place.state}</span>
+//     </li>
+//     `;
+//   });
 }
-
-// findMatches();
